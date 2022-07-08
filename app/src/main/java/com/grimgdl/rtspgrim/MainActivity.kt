16f708e9f9f88820bc92047d9c603a57dd8eb99f
Gustavo Ibarra <gustavoibarra1989@gmail.com>
2022-07-08 17:55:15 -0500

    added button to start streaming

--- app/src/main/java/com/grimgdl/rtspgrim/MainActivity.kt
+++ app/src/main/java/com/grimgdl/rtspgrim/MainActivity.kt
@@ -1,15 +1,10 @@
 package com.grimgdl.rtspgrim
 
-import android.graphics.Insets
-import android.graphics.Point
-import android.graphics.Rect
+
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
-import android.util.Size
-import android.view.WindowInsets
-import android.view.WindowManager
+
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.grimgdl.rtspgrim.databinding.MainActivityLayoutBinding
@@ -18,11 +13,13 @@ import org.videolan.libvlc.Media
 import org.videolan.libvlc.MediaPlayer
 import org.videolan.libvlc.interfaces.IVLCVout
 
+
 class MainActivity: AppCompatActivity(), IVLCVout.Callback {
 
 
     private lateinit var mediaPlayer : MediaPlayer
     private lateinit var vlc: LibVLC
+
 
 
     @RequiresApi(Build.VERSION_CODES.R)
@@ -38,15 +35,12 @@ class MainActivity: AppCompatActivity(), IVLCVout.Callback {
         val vlcVideoLayout = binding.videoLayout
         mediaPlayer = MediaPlayer(vlc)
 
-        mediaPlayer.vlcVout.setVideoSurface(vlcVideoLayout.holder.surface,  vlcVideoLayout.holder)
-        //mediaPlayer.vlcVout.attachViews()
 
+        mediaPlayer.vlcVout.setVideoSurface(vlcVideoLayout.holder.surface,  vlcVideoLayout.holder)
 
         val ivlcVout = mediaPlayer.vlcVout
         ivlcVout.setWindowSize(1280, 760)
-        ivlcVout.attachViews();
-
-
+        ivlcVout.attachViews()
 
 
         val connectionData = String.format("rtsp://%s:%s@%s:%s/cam/realmonitor?channel=1&subtype=0",
@@ -54,11 +48,24 @@ class MainActivity: AppCompatActivity(), IVLCVout.Callback {
 
         val media = Media(vlc, Uri.parse(connectionData))
 
+
         mediaPlayer.media = media
         media.release()
 
+        mediaPlayer.setEventListener {
 
-        mediaPlayer.play()
+            binding.bitrate.post {
+                binding.bitrate.text = it.lengthChanged.toString()
+            }
+
+
+        }
+
+
+        binding.play.setOnClickListener{
+            if (mediaPlayer.isPlaying) mediaPlayer.stop()
+            else mediaPlayer.play()
+        }
 
     }
 
@@ -86,23 +93,6 @@ class MainActivity: AppCompatActivity(), IVLCVout.Callback {
         mediaPlayer.stop()
         mediaPlayer.detachViews()
 
-    }
-
-    fun WindowManager.currentWindowMetricsPointCompat(): Point {
-        return if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
-            val windowInsets = currentWindowMetrics.windowInsets
-            var insets: Insets = windowInsets.getInsets(WindowInsets.Type.navigationBars())
-            windowInsets.displayCutout?.run {
-                insets = Insets.max(insets, Insets.of(safeInsetLeft, safeInsetTop, safeInsetRight, safeInsetBottom))
-            }
-            val insetsWidth = insets.right + insets.left
-            val insetsHeight = insets.top + insets.bottom
-            Point(currentWindowMetrics.bounds.width() - insetsWidth, currentWindowMetrics.bounds.height() - insetsHeight)
-        }else{
-            Point().apply {
-                defaultDisplay.getSize(this)
-            }
-        }
     }
 
 
